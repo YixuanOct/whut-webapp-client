4b1931eec8a94409c4fa3e20be3c63f5d9d23032
YixuanOct <yixuanoct@gmail.com>
2025-03-11 18:59:24 +0800

    feat(nav): implement sidebar nav and theme switching
    - Implement sidebar navigation
    - Emit event to notify parent component to switch theme

--- src/components/SidebarNav.vue
+++ src/components/SidebarNav.vue
@@ -1,1 +1,105 @@
-<template></template>
+<script setup>
+import { ref } from "vue";
+import {
+  HomeOutlined,
+  CompassOutlined,
+  SkinOutlined,
+  EnvironmentOutlined,
+  UserOutlined,
+  ReadOutlined,
+  HighlightTwoTone,
+} from "@ant-design/icons-vue";
+
+const isCollapse = ref(false);
+</script>
+
+<template>
+  <el-menu
+    :collapse="isCollapse"
+    :default-openeds="['2', '3']"
+    default-active="1"
+  >
+    <el-menu-item index="1">
+      <HomeOutlined class="icon" />
+      <span>首页</span>
+    </el-menu-item>
+    <el-sub-menu index="2">
+      <template #title>
+        <CompassOutlined class="icon" />
+        <span>功能菜单</span>
+      </template>
+      <el-menu-item index="2-1">
+        <UserOutlined class="icon" />
+        <span>用户管理</span>
+      </el-menu-item>
+      <el-menu-item index="2-2">
+        <ReadOutlined class="icon" />
+        <span>文章管理</span>
+      </el-menu-item>
+    </el-sub-menu>
+    <el-sub-menu index="3">
+      <template #title>
+        <SkinOutlined class="icon" />
+        <span>主题切换</span>
+      </template>
+      <el-menu-item @click="$emit('toggleBlueTheme')">
+        <HighlightTwoTone class="icon" twoToneColor="#409EFF" />
+        <span>蓝色风格</span>
+      </el-menu-item>
+      <el-menu-item @click="$emit('toggleYellowTheme')">
+        <HighlightTwoTone class="icon" twoToneColor="#E6A23C" />
+        <span>黄色风格</span>
+      </el-menu-item>
+    </el-sub-menu>
+    <el-menu-item index="4">
+      <EnvironmentOutlined class="icon" />
+      <span>导航三</span>
+    </el-menu-item>
+    <el-menu-item index="5">
+      <EnvironmentOutlined class="icon" />
+      <span>导航四</span>
+    </el-menu-item>
+  </el-menu>
+</template>
+
+<style scoped>
+.icon {
+  margin-right: 20px;
+}
+
+.blue-theme .el-menu-item.is-active {
+  color: var(--el-color-primary);
+}
+
+.blue-theme :deep(.el-sub-menu__title),
+.blue-theme .el-menu-item:not(.el-sub-menu .el-menu-item) {
+  background-color: var(--el-color-primary-light-5);
+}
+
+.blue-theme :deep(.el-sub-menu__title:hover),
+.blue-theme .el-menu-item:hover {
+  background-color: var(--el-color-primary-light-9) !important;
+}
+
+.blue-theme .el-menu-item.is-active {
+  background-color: var(--el-color-primary-light-9) !important;
+}
+
+.yellow-theme .el-menu-item.is-active {
+  color: var(--el-color-warning);
+}
+
+.yellow-theme :deep(.el-sub-menu__title),
+.yellow-theme .el-menu-item:not(.el-sub-menu .el-menu-item) {
+  background-color: var(--el-color-warning-light-5);
+}
+
+.yellow-theme :deep(.el-sub-menu__title:hover),
+.yellow-theme .el-menu-item:hover {
+  background-color: var(--el-color-warning-light-9) !important;
+}
+
+.yellow-theme .el-menu-item.is-active {
+  background-color: var(--el-color-warning-light-9) !important;
+}
+</style>
--- src/layouts/HomeLayout.vue
+++ src/layouts/HomeLayout.vue
@@ -1,14 +1,20 @@
 <script setup>
 import SidebarNav from "@/components/SidebarNav.vue";
 import { ref } from "vue";
+
+const theme = ref("blue-theme");
 </script>
 
 <template>
   <el-container class="home-container">
-    <el-aside width="250px">
-      <SidebarNav />
+    <el-aside width="200px">
+      <SidebarNav
+        :class="theme"
+        @toggle-blue-theme="() => (theme = 'blue-theme')"
+        @toggle-yellow-theme="() => (theme = 'yellow-theme')"
+      />
     </el-aside>
-    <el-container class="home-sub-container">
+    <el-container class="home-sub-container" :class="theme">
       <el-header class="home-header"> </el-header>
       <el-main class="home-main">
         <router-view v-slot="{ Component }">
@@ -32,11 +38,19 @@ import { ref } from "vue";
   height: 100%;
 }
 
-.home-header {
+.blue-theme .home-header {
   background-color: var(--el-color-primary-dark-2);
 }
 
-.home-main {
+.blue-theme .home-main {
   background-color: var(--el-color-primary-light-8);
+}
+
+.yellow-theme .home-header {
+  background-color: var(--el-color-warning-dark-2);
+}
+
+.yellow-theme .home-main {
+  background-color: var(--el-color-warning-light-8);
 }
 </style>
