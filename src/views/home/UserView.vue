565513b7fb4716fead5bc767581b77fcd73ab2b7
YixuanOct <yixuanoct@gmail.com>
2025-03-14 18:20:54 +0800

    ✨ feat(user-view): implement user management interface
    - add script setup with table data generation and pagination logic
    - create user management layout with header, main content, and footer
    - implement table component with columns and actions
    - add search input and buttons in header
    - style components with proper spacing and layout
    - add pagination component with size and page change handlers

--- src/views/home/UserView.vue
+++ src/views/home/UserView.vue
@@ -1,12 +1,182 @@
+<script setup>
+import { PlusOutlined, SearchOutlined } from "@ant-design/icons-vue";
+import { ref, computed, onMounted } from "vue";
+
+// const tableData = [
+//   {
+//     id: "1",
+//     date: "2016-05-03 08:00:00",
+//     name: "王小虎",
+//     province: "上海",
+//     city: "普陀区",
+//     address: "上海市普陀区金沙江路 1518 弄",
+//     postcode: "200333",
+//   },
+//   {
+//     id: "2",
+//     date: "2016-05-03 08:00:00",
+//     name: "张小虎",
+//     province: "上海",
+//     city: "普陀区",
+//     address: "上海市普陀区金沙江路 1518 弄",
+//     postcode: "200333",
+//   },
+//   {
+//     id: "3",
+//     date: "2016-05-03 08:00:00",
+//     name: "刘小虎",
+//     province: "上海",
+//     city: "普陀区",
+//     address: "上海市普陀区金沙江路 1518 弄",
+//     postcode: "200333",
+//   },
+//   {
+//     id: "4",
+//     date: "2016-05-03 08:00:00",
+//     name: "李小虎",
+//     province: "上海",
+//     city: "普陀区",
+//     address: "上海市普陀区金沙江路 1518 弄",
+//     postcode: "200333",
+//   },
+//   {
+//     id: "5",
+//     date: "2016-05-03 08:00:00",
+//     name: "赵小虎",
+//     province: "上海",
+//     city: "普陀区",
+//     address: "上海市普陀区金沙江路 1518 弄",
+//     postcode: "200333",
+//   },
+//   {
+//     id: "6",
+//     date: "2016-05-03 08:00:00",
+//     name: "吴小虎",
+//     province: "上海",
+//     city: "普陀区",
+//     address: "上海市普陀区金沙江路 1518 弄",
+//     postcode: "200333",
+//   },
+// ];
+const tableData = [];
+for (let i = 0; i < 100; i++) {
+  tableData.push({
+    id: i + 1,
+    date: "2016-05-03 08:00:00",
+    name: "王小虎",
+    province: "上海",
+    city: "普陀区",
+    address: "上海市普陀区金沙江路 1518 弄",
+  });
+}
+const currentPage = ref(1);
+const pageSize = ref(5);
+const total = computed(() => tableData.length);
+const tableDataView = computed(() => {
+  return tableData.slice(
+    (currentPage.value - 1) * pageSize.value,
+    currentPage.value * pageSize.value
+  );
+});
+const keyword = ref("");
+
+function handleSizeChange(val) {}
+
+function handleCurrentChange(val) {}
+</script>
+
 <template>
-  <el-container>
-    <el-header></el-header>
-    <el-main class="user-main"></el-main>
+  <el-container class="user-container">
+    <el-header class="user-header">
+      <h1 style="margin-bottom: 3rem">用户管理</h1>
+      <div class="table-header">
+        <el-button plain type="primary" style="height: 40px">
+          <PlusOutlined class="icon" />
+          新增
+        </el-button>
+        <el-input
+          v-model="keyword"
+          placeholder="请输入昵称"
+          :prefix-icon="SearchOutlined"
+        />
+        <el-button type="primary" style="height: 40px">
+          <PlusOutlined class="icon" />
+          搜索
+        </el-button>
+      </div>
+    </el-header>
+    <el-main class="user-main">
+      <el-table
+        :data="tableDataView"
+        style="width: 100%"
+        :show-overflow-tooltip="true"
+        :row-style="{ height: '48px' }"
+        :header-row-style="{ height: '48px' }"
+        height="100%"
+        border
+      >
+        <el-table-column prop="id" label="序号" width="100" />
+        <el-table-column prop="date" label="日期" width="200" />
+        <el-table-column prop="name" label="姓名" width="100" />
+        <el-table-column prop="province" label="省份" width="150" />
+        <el-table-column prop="city" label="市区" width="100" />
+        <el-table-column prop="address" label="地址" min-width="250" />
+        <el-table-column prop="postcode" label="邮编" width="100" />
+        <el-table-column fixed="right" label="操作" width="100">
+          <template #default>
+            <el-button link type="primary" size="small">编辑</el-button>
+            <el-button link type="primary" size="small">删除</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-main>
+    <el-footer class="user-footer">
+      <el-pagination
+        v-model:current-page="currentPage"
+        v-model:page-size="pageSize"
+        :page-sizes="[5, 10, 15, 20]"
+        layout="total, sizes, prev, pager, next"
+        :total="total"
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+      />
+    </el-footer>
   </el-container>
 </template>
 
 <style scoped>
-.user-main {
+.user-container {
   background-color: white;
+  height: 100%;
+}
+.user-header {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: auto;
+}
+.user-main {
+  padding-top: 2px;
+}
+.icon {
+  margin-right: 4px;
+}
+.table-header {
+  display: flex;
+  width: 100%;
+}
+.el-input {
+  width: 250px;
+  margin-left: auto;
+  margin-right: 4px;
+}
+.user-footer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.table-body {
+  height: calc(100%-40px);
 }
 </style>
